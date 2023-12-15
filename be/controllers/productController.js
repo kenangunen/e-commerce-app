946b87cd98dcd8be663c61491260711e90d04692
kenangunen <gunenkenan@gmail.com>
2023-12-15 20:03:25 +0300

    fix: codeformat

--- be/controllers/productController.js
+++ be/controllers/productController.js
@@ -1,84 +1,80 @@
-const Procust = require('../models/Product');
-const { notFoundError } = require('../utils/errors')
+const Product = require('../models/Product');
+const { notFoundError } = require('../utils/errors');
 
 // fetch all products
 const getAllProducts = async (req, res, next) => {
-    try {
-        const products = await Product.find()
-        res.json(products);
-    } catch (error) {
-        next(error);
-    }
-}
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    next(error);
+  }
+};
 
 // fetch a specific product
 const getProductById = async (req, res, next) => {
-    const product = req.param.productId;
-    try {
-        const product = await Product.findById(productId);
+  const productId = req.param.productId;
+  try {
+    const product = await Product.findById(productId);
 
-        notFoundError(productId, 'product');
+    notFoundError(productId, 'product');
 
-        res.json(product);
-    } catch (error) {
-        next(error);
-    }
-}
+    res.json(product);
+  } catch (error) {
+    next(error);
+  }
+};
 
 // add a new product
 const addProduct = async (req, res, next) => {
-    const { name, price, description } = req.body;
-    //TODO: use JOI for validation req data.
-    const newProduct = new Product({ name, price, description });
+  const { name, price, description } = req.body;
+  //TODO: use JOI for validation req data.
+  const newProduct = new Product({ name, price, description });
 
-    try {
-        const savedProduct = await newProduct.save();
-        res.status(201).json(savedProduct);
-    } catch (error) {
-        next(error);
-    }
-}
+  try {
+    const savedProduct = await newProduct.save();
+    res.status(201).json(savedProduct);
+  } catch (error) {
+    next(error);
+  }
+};
 
 // update a product
 const updateProduct = async (req, res, next) => {
-    const productId = req.params.productId;
-    const { name, price, description } = req.body;
-    //TODO: use JOI for validation req data.
+  const productId = req.params.productId;
+  const { name, price, description } = req.body;
+  //TODO: use JOI for validation req data.
 
-    try {
-        const updatedProduct = await Product.findByIdAndUpdate(
-            productId,
-            { name, price, description },
-            { new: true }
-        );
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(productId, { name, price, description }, { new: true });
 
-        notFoundError(updatedProduct, 'updatedProduct');
+    notFoundError(updatedProduct, 'updatedProduct');
 
-        res.json(updatedProduct);
-    } catch (error) {
-        next(error);
-    }
+    res.json(updatedProduct);
+  } catch (error) {
+    next(error);
+  }
 };
 
 // delete a product
 const deleteProduct = async (req, res, next) => {
-    const productId = req.params.productId;
+  const productId = req.params.productId;
 
-    try {
-        const deletedProduct = await Product.findByIdAndDelete(productId);
+  try {
+    const deletedProduct = await Product.findByIdAndDelete(productId);
 
-        notFoundError(deletedProduct, 'deletedProduct');
+    notFoundError(deletedProduct, 'deletedProduct');
 
-        res.json(deletedProduct);
-    } catch (error) {
-        next(error);
-    }
-}
+    res.json(deletedProduct);
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = {
-    getAllProducts,
-    getProductById,
-    addProduct,
-    updateProduct,
-    deleteProduct
-}
+  getAllProducts,
+  getProductById,
+  addProduct,
+  updateProduct,
+  deleteProduct
+};
