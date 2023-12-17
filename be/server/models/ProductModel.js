fd75e3a1156c5b86eeebe61a165b75706ebed87f
kenangunen <gunenkenan@gmail.com>
2023-12-17 18:40:49 +0300

    feat: add virtuals to productSchema

--- be/server/models/ProductModel.js
+++ be/server/models/ProductModel.js
@@ -1,37 +1,75 @@
 const mongoose = require('mongoose');
 const { randomUUID } = require('crypto');
 
-const productSchema = new mongoose.Schema({
-  productId: {
-    type: 'UUID',
-    default: () => randomUUID()
+const productSchema = new mongoose.Schema(
+  {
+    // defined fields
+    productId: {
+      type: String,
+      default: () => randomUUID()
+    },
+    productName: {
+      type: String,
+      required: [true, '{PATH} alanı zorunludur.'],
+      maxlength: [50, '{PATH}  alanı (`{VALUE}`), ({MAXLENGTH}) karakterden küçük olmalıdır.'],
+      minlength: [3, '{PATH}  alanı (`{VALUE}`), ({MINLENGTH}) karakterden büyük olmalıdır. ']
+    },
+    colorName: {
+      // collorId tut, color tablosundan isimlerini al.
+      type: String,
+      required: [true, '{PATH} alanı zorunludur.']
+    },
+    sellerName: {
+      // sellerId tut, seller tablosundan isimlerini al.
+      type: String,
+      required: [true, '{PATH} alanı zorunludur.']
+    },
+    brandName: {
+      // brandId tut, brand tablosundan isimlerini al.
+      type: String,
+      required: [true, '{PATH} alanı zorunludur.']
+    },
+    categoryName: {
+      type: String,
+      minlength: [3, 'Kategori alanı minimum {MINLENGTH} karakter olabilir.'],
+      maxlength: 30
+    },
+    price: {
+      type: Number,
+      required: [true, '{PATH} alanı zorunludur.']
+    },
+    description: {
+      type: String,
+      maxlength: [200, '{PATH}  alanı (`{VALUE}`), ({MAXLENGTH}) karakterden küçük olmalıdır.'],
+      required: [true, '{PATH} alanı zorunludur.']
+    },
+    stock: {
+      type: Number,
+      required: [true, '{PATH} alanı zorunludur.']
+    },
+    images: [String],
+    addedDate: {
+      type: Date,
+      default: Date.now()
+    }
   },
-  name: {
-    type: String,
-    required: [true, '{PATH} alanı zorunludur.'],
-    maxlength: [50, '{PATH}  alanı (`{VALUE}`), ({MAXLENGTH}) karakterden küçük olmalıdır.'],
-    minlength: [3, '{PATH}  alanı (`{VALUE}`), ({MINLENGTH}) karakterden büyük olmalıdır. ']
-  },
-  categoryName: {
-    type: String,
-    minlength: [3, 'Kategori alanı minimum {MINLENGTH} karakter olabilir.'],
-    maxlength: 30
-  },
-  price: {
-    type: Number,
-    required: [true, '{PATH} alanı zorunludur.']
-  },
-  description: {
-    type: String,
-    maxlength: [200, '{PATH}  alanı (`{VALUE}`), ({MAXLENGTH}) karakterden küçük olmalıdır.'],
-    required: [true, '{PATH} alanı zorunludur.']
-  },
-  stock: {
-    type: Number,
-    required: [true, '{PATH} alanı zorunludur.']
-  },
-  images: [String]
-});
+  {
+    toJSON: { virtuals: true },
+    virtuals: {
+      fullProductName: {
+        get() {
+          return this.brandName + '-' + this.productName + '-' + this.colorName;
+        },
+        set(value) {
+          const parts = value.split('-');
+          this.brandName = parts[0].trim();
+          this.productName = parts[1].trim();
+          this.colorName = parts[2].trim();
+        }
+      }
+    }
+  }
+);
 
 const Product = mongoose.model('Product', productSchema);
 
